73c00ff924091e249eeb8b696a5687c1199a819f
Danvintius <danila.94@mail.ru>
2023-10-09 01:48:37 +0300

    upd

---
+++ test.js
@@ -0,0 +1,4 @@
+let paren = document.createElement('div')
+let body = document.querySelector('.test')
+body.appendChild(paren)
+paren.textContent = 'Text pext next'
---
+++ test.ts
@@ -0,0 +1,9 @@
+class User {
+    name: string;
+    role: string
+    
+    constructor(name: string, role: string) {
+        this.name = name,
+        this.role = role
+    }
+}
